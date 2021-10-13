26a79c7a65378cb74753fe4b3ce6f7b325ae982c
Daniele Mauro <danima82@gmail.com>
2021-10-13 18:09:53 +0200

    fix boiler card style

--- src/component/boiler.js
+++ src/component/boiler.js
@@ -3,12 +3,12 @@ import boilerImg from '../images/boiler.png';
 
 
 export default function Boiler() {
-    return (
-        <div className='row'>
-            <div className='col-4'>
+    return (<>
+        <div className='row mb-5 g-2'>
+            <div className='col-3'>
                 <div className='boilerCard'>
                     <span className='sale'>Risparmi il 20%</span>
-                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
                     <img
                         src={boilerImg}
                         alt="Boiler"
@@ -16,39 +16,304 @@ export default function Boiler() {
                     <div className='boilerData'>
                         <span className='boilerBrand'>Ariston</span><br />
                         <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
-                        <span className='boilerDiscountPrice'>1.350,00 €</span> <span className='boilerPrice'>1.570,00 €</span><br />
-                        <span className='boilerService'>Sopralluogo e installazione inclusi</span><br />
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
                         <hr />
-                        <span>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
                         </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
                     </div>
                 </div>
             </div>
-            <div className='col-4'>
+            <div className='col-3'>
                 <div className='boilerCard'>
                     <span className='sale'>Risparmi il 20%</span>
-                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
                     <img
                         src={boilerImg}
                         alt="Boiler"
                     />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
                 </div>
             </div>
-            <div className='col-4'>
+            <div className='col-3'>
                 <div className='boilerCard'>
                     <span className='sale'>Risparmi il 20%</span>
-                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
                     <img
                         src={boilerImg}
                         alt="Boiler"
                     />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div className='row mb-5'>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='row mb-5'>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='row mb-5'>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+            <div className='col-3'>
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i className="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='prezzo'>1.350,00 €</span> <span className='prezzoPreSconto'>1.570,00 €</span><br />
+                        <span className='note'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span className='stars'>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </span>
+                        <span className='confronta'>Confronta <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
     );
 }
