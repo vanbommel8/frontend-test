2f41f04109a0928a294fe4697ca9c5de0c0d0b9a
Daniele Mauro <danima82@gmail.com>
2021-10-13 16:05:59 +0200

    add styles boiler cards

--- src/component/boiler.js
+++ src/component/boiler.js
@@ -6,22 +6,48 @@ export default function Boiler() {
     return (
         <div className='row'>
             <div className='col-4'>
-                <img
-                    src={boilerImg}
-                    alt="Boiler"
-                />
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                    <div className='boilerData'>
+                        <span className='boilerBrand'>Ariston</span><br />
+                        <div className='boilerDesc'>Matis condens - Condensing Boiler 24 kW Methane - Indoor</div>
+                        <span className='boilerDiscountPrice'>1.350,00 €</span> <span className='boilerPrice'>1.570,00 €</span><br />
+                        <span className='boilerService'>Sopralluogo e installazione inclusi</span><br />
+                        <hr />
+                        <span>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </span>
+                    </div>
+                </div>
             </div>
             <div className='col-4'>
-                <img
-                    src={boilerImg}
-                    alt="Boiler"
-                />
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                </div>
             </div>
             <div className='col-4'>
-                <img
-                    src={boilerImg}
-                    alt="Boiler"
-                />
+                <div className='boilerCard'>
+                    <span className='sale'>Risparmi il 20%</span>
+                    <span className='wishlist'><i class="far fa-heart"></i></span>
+                    <img
+                        src={boilerImg}
+                        alt="Boiler"
+                    />
+                </div>
             </div>
         </div>
     );
