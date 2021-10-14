1fe5fd283294fb3746762619fb577040d5f61f6b
Santo <90331134+lastdark@users.noreply.github.com>
2021-10-14 14:50:44 +0200

    state fix

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,18 +1,11 @@
 
 
-export  default  const INITIAL_STATE = {
+export  const INITIAL_STATE = {
     boiler: {
         boilers: [],
-        totalPrice: 0,
-        quantity: 0,
-        opened: false,
+      
     },
-    filters: {
-        word: "",
-        category: "All",
-        title:'',
-        filterdBooks:booksMock
-    },
+   
      boilersStock: {
 
         brands:'',
