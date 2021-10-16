14dcd375149c51853c96c1171bc04ab31aa717ae
Santo <90331134+lastdark@users.noreply.github.com>
2021-10-16 14:32:30 +0200

    fix ui

--- src/component/shared/ControntaAlert.js
+++ src/component/shared/ControntaAlert.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 export default function ConfrontaAlert({showBar}) {
+
+
+    const title = () => {
+        switch (showBar.length) {
+            case 1:
+                return 'Seleziona altri 2 prodotti per confrontarli'  ;
+            case 2:
+                return 'Seleziona un altro  prodotti per confrontarli' ;
+            case 3 :
+                return 'Confronta';
+
+
+            default:
+                return 'Hai Selezionato troppi prodotti  (max 3)';
+        }
+    };
     return (
-        (showBar.length>=1) &&
+        (showBar.length>0) &&
         <div className='confrontaAlert'>
-            <span className='testoConfronta'>Seleziona altri 2 prodotti per confrontarli</span>
-            <button disabled={1?true:false} className='buttonConfronta'>CONFRONTA</button>
+            <span className='testoConfronta'>{title()}</span>
+            <button disabled={ (showBar.length<3) ||( showBar.length>=4)?true:false} className='buttonConfronta'>CONFRONTA</button>
         </div>
     )
 }
